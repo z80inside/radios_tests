62bde80dc4f16ec87bc68902c4730a29eb9ec898
Richi <ricardo.canuelo@gmail.com>
2011-09-13 12:18:11 +0200

    Added device reading and writing in flash

--- CC1110/source/apps/simp_app.c
+++ CC1110/source/apps/simp_app.c
@@ -6,6 +6,12 @@
 #include "bsp_buttons.h"
 #include "bsp_extended.h"
 #include "uart_intfc.h"
+
+#include "ioCC1110.h"
+#include "ioCCxx10_bitdef.h"
+#include "hal_types.h"
+#include "dma.h"
+
 #include <stdlib.h>
 
 /*
@@ -35,19 +41,25 @@
 
 #define RADIO_MAX_TICKS 10
 
+/* One whole page of flash memory is to be reserved, i.e., 1 KiB. */
+#define PAGE_SIZE 1024
+
+/* Device identifier length (exluding the terminal '\0'). */
+#define IDSIZE 4
+
 /*
 * GLOBAL VARIABLES
 */
 static linkID_t sLinkID;
 static volatile uint8_t rxFlag;
 static volatile uint8_t instFlag;
-static uint8_t sCurrentPwrLevel;
-static uint8_t sRequestPwrLevel;
-static uint8_t sNoAckCount = 0;
+//static uint8_t sCurrentPwrLevel;
+//static uint8_t sRequestPwrLevel;
+//static uint8_t sNoAckCount = 0;
 static uint8_t ins_ini = 0;
 static uint8_t ins_end = 0;
-static uint8_t loopticks = 0;
-static uint8_t radio_on = 1;
+//static uint8_t loopticks = 0;
+//static uint8_t radio_on = 1;
 
 struct ringbuf {
 	char buffer[20];
@@ -56,6 +68,19 @@ struct ringbuf {
 	uint8_t tail;
 	uint8_t items;
 };
+
+static char device_id[IDSIZE];
+
+/* The area in flash where the string (data written to flash) will be placed.
+ * If not placed at an even absolute location, the address might be odd (e.g.
+ * 0x4401). Since flash is addressed in words, writing would start at one byte
+ * before (or after) the address unless explicit action is made to avoid this.
+ * Page 17 will be used, so the address is 0x4400 ( = 1024 * 17).
+ */
+__no_init const char __code flashDataAddr[PAGE_SIZE] @ 0x4400;
+
+/* DMA configuration descriptor used for flash operations */
+static DMA_DESC dmaConfig0;
 
 /*
 * LOCAL FUNCTIONS
@@ -68,11 +93,16 @@ static void check_instructions(struct ringbuf *b);
 static void init_inst_buf(struct ringbuf*);
 static void append_data(struct ringbuf *buf_out, char *buf_in, uint8_t n);
 static void print_rx_status(struct ringbuf *b);
+void writeFlashDMA(char *data, uint8_t len);
+int check_id(void);
 
+/* Implemented in assembly */
+void halFlashStartErase(void);
+void halFlashStartWrite(void);
+
+/* Entry point */
 void main (void)
-{
-	char rxByte = 0;
-	
+{	
 	BSP_Init();
 
 	/* Init UART */
@@ -82,11 +112,11 @@ void main (void)
 	addr_t lAddr;
 	BSP_createRandomAddress(&lAddr);
 	SMPL_Ioctl(IOCTL_OBJ_ADDR, IOCTL_ACT_SET, &lAddr);
+	
+	/* Read the device identifier from flash memory */
 
 	/* Initialize SimpliciTI and provide Callback function */
 	SMPL_Init(sRxCallback);
-
-	tx_send_wait("Send-Receive demo\r\n", sizeof("Send-Receive demo\r\n"));
 
 	/* Turn on LEDs indicating power on */
 	BSP_TURN_ON_LED1();
@@ -97,6 +127,10 @@ void main (void)
 	BSP_TURN_OFF_LED1();
 	BSP_TURN_OFF_LED2();
 
+	if (!check_id()) {
+		writeFlashDMA("1234", 4);
+	}
+	
 	wait_for_link();
 
 	tx_send_wait("Send a number enclosed in ? signs\r\n",
@@ -128,9 +162,9 @@ static void main_loop()
 	uint8_t in_rx = 0;
 	uint8_t chars_read = 0;
 	char buf[15];
-	uint8_t rcvmsg[5];
-	uint8_t len;
-	uint8_t i;
+	//uint8_t rcvmsg[5];
+	//uint8_t len;
+	//uint8_t i;
 	
 	init_inst_buf(&inst_buf);
 	while (1) {
@@ -188,7 +222,6 @@ static void print_rx_status(struct ringbuf *b)
 	uint8_t head = 48 + b->head;
 	uint8_t tail = 48 + b->tail;
 	uint8_t items = 48 + b->items;
-	uint8_t i;
 
 	tx_send("\r\nhead: ", sizeof("\r\nhead: "));
 	tx_send(&head, 1);
@@ -307,4 +340,102 @@ static uint8_t sRxCallback(linkID_t lid)
 	}
 	/* Leave frame to be read by application. */
 	return 0;
+}
+
+void writeFlashDMA(char *data, uint8_t len)
+{
+	/* Configure DMA channel 0. Settings:
+	* SRCADDR: address of the data to be written to flash (increasing).
+	* DESTADDR: the flash controller data register (fixed), so that the
+	*     flash controller will write this data to flash.
+	* VLEN: use LEN for transfer count.
+	* LEN: equal to the number of bytes to be transferred.
+	* WORDSIZE: each transfer should transfer one byte.
+	* TMODE: should be set to single mode (see datasheet, DMA Flash Write).
+	*     Each flash write complete will re-trigger the DMA channel.
+	* TRIG: let the DMA channel be triggered by flash data write complete
+	   (trigger number 18). That is, the flash controller will trigger the
+	   DMA channel when the Flash Write Data register, FWDATA, is ready to
+	   receive new data.
+	* SRCINC: increment by one byte.
+	* DESTINC: fixed (always write to FWDATA).
+	* IRQMASK: disable interrupts from this channel.
+	* M8: 0, irrelevant since we use LEN for transfer count.
+	* PRIORITY: high.
+	*/
+	dmaConfig0.SRCADDRH  = ((uint16)data >> 8) & 0x00FF;
+	dmaConfig0.SRCADDRL  = (uint16)data & 0x00FF;
+	dmaConfig0.DESTADDRH = ((uint16)&X_FWDATA >> 8) & 0x00FF;
+	dmaConfig0.DESTADDRL = (uint16)&X_FWDATA & 0x00FF;
+	dmaConfig0.VLEN      = DMA_VLEN_USE_LEN;
+	dmaConfig0.LENH      = (len >> 8) & 0x00FF;
+	dmaConfig0.LENL      = len & 0x00FF;
+	dmaConfig0.WORDSIZE  = DMA_WORDSIZE_BYTE;
+	dmaConfig0.TMODE     = DMA_TMODE_SINGLE;
+	dmaConfig0.TRIG      = DMA_TRIG_FLASH;
+	dmaConfig0.SRCINC    = DMA_SRCINC_1;
+	dmaConfig0.DESTINC   = DMA_DESTINC_0;
+	dmaConfig0.IRQMASK   = DMA_IRQMASK_DISABLE;
+	dmaConfig0.M8        = DMA_M8_USE_8_BITS;
+	dmaConfig0.PRIORITY  = DMA_PRI_HIGH;
+	
+	/* The DMA configuration data structure may reside at any location in
+	* unified memory space, and the address location is passed to the DMA
+	* through DMA0CFGH:DMA0CFGL.
+	*/
+	DMA0CFGH = ((uint16)&dmaConfig0 >> 8) & 0x00FF;
+	DMA0CFGL = (uint16)&dmaConfig0 & 0x00FF;
+	
+	/* Waiting for the flash controller to be ready */
+	while (FCTL & FCTL_BUSY);
+	
+	/* Configuring the flash controller. Setings:
+	* FWT: 0x11 (default setting, matches 13 MHz clock frequency).
+	* FADDRH:FADDRL: point to the area in flash to write to - flashDataAddr.
+	*/
+	FWT = 0x11;
+	FADDRH = (int)flashDataAddr >> 9;
+	FADDRL = ((int)flashDataAddr >> 1) & ~0xFF00;
+	
+	/* Erase the page that will be written to. */
+	halFlashStartErase();
+	
+	/* Wait for the erase operation to complete. */
+	while (FCTL & FCTL_BUSY);
+	
+	/* Arm the DMA channel, so that a DMA trigger will initiate DMA writing. */
+	DMAARM |= DMAARM0;
+	
+	/* Enable flash write. Generates a DMA trigger. Must be aligned on a 2-byte
+	* boundary and is therefor implemented in assembly.
+	*/
+	halFlashStartWrite();
+	
+	/* Wait for DMA transfer to complete. */
+	while (!(DMAIRQ & DMAIRQ_DMAIF0));
+	
+	/* Wait until flash controller not busy. */
+	while (FCTL & (FCTL_BUSY | FCTL_SWBSY));
+	
+	/* By now, the transfer is completed, so the transfer count is reached.
+	* The DMA channel 0 interrupt flag is then set, so we clear it here.
+	*/
+	DMAIRQ &= ~DMAIRQ_DMAIF0;
+}
+
+int check_id()
+{
+	uint8_t i;
+	uint8_t ret = 1;
+	
+	tx_send("Device id: ", sizeof("Device id: "));
+	for (i = 0; i < IDSIZE; i++) {
+		device_id[i] = flashDataAddr[i];
+		tx_send(&device_id[i], 1);
+		if (device_id[i] < 48 || device_id[i] > 57)
+			ret = 0;
+	}
+	tx_send("\r\n", 2);
+	
+	return ret;
 }
